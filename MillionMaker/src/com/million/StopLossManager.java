7a61a2e0d6d747e2063cdcf7dce8496e3933ba32
Jayander <Jayander@gmail.com>
2018-03-28 18:27:57 +0530

    New files

---
+++ MillionMaker/src/com/million/StopLossManager.java
@@ -0,0 +1,53 @@
+package com.million;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import org.apache.log4j.Logger;
+
+import com.million.config.WealthConfig;
+import com.million.kite.login.KiteHelper;
+import com.rainmatter.kitehttp.exceptions.KiteException;
+
+public class StopLossManager {
+
+	private static Logger logger = Logger.getLogger(StopLossManager.class);
+	
+	public static void main(String[] args) {
+		
+		
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(
+				WealthConfig.getInstance().getProperty("EXISTING_POSITIONS"))))){
+			
+			KiteHelper kiteHelper = new KiteHelper();
+			String line;
+			
+			while ((line = bufferedReader.readLine()) != null) {
+				
+				if(line.startsWith("SCRIP_NAME"))	{
+					continue;
+				}
+				
+				String []values = line.split(",");
+				String scripName = values[0].trim();
+				
+				int quantity = Integer.valueOf(values[1].trim());
+				
+				float stopLossPrice = Float.valueOf(values[2]);
+				
+				try {
+					kiteHelper.placeSLSellOrder("NSE", scripName, stopLossPrice, quantity, stopLossPrice);
+				} catch (KiteException e) {
+					logger.error("Exception occured ", e);
+				}
+				
+			}
+		} catch (Exception e) {
+			logger.error("Exception occured ", e);
+			System.exit(-1);
+		}
+
+	}
+
+}
