ec20ef4becf69cf210a23553381481a075019ce2
Jayander <Jayander@gmail.com>
2018-04-01 22:36:43 +0530

    NEW FILE

---
+++ MillionMaker/stopLossManager.bat
@@ -0,0 +1,9 @@
+set BASE_DIR=C:/Users/Jayander/git/MillionMaker/MillionMaker
+
+set CLASSPATH=%CLASSPATH%/;%BASE_DIR%/bin;%BASE_DIR%/lib/*
+
+cd %BASE_DIR%
+
+echo on
+
+java com.million.StopLossManager
