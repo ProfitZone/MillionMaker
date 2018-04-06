448a2b4d4566a9e73babbde8f365f56355217e71
Jayander <Jayander@gmail.com>
2018-04-06 19:57:38 +0530

    new file

---
+++ MillionMaker/validateWinners.bat
@@ -0,0 +1,16 @@
+set BASE_DIR=C:/Users/Jayander/git/MillionMaker/MillionMaker
+
+set CLASSPATH=%CLASSPATH%/;%BASE_DIR%/bin;%BASE_DIR%/lib/*
+
+cd %BASE_DIR%
+
+echo on
+
+del C:\Users\Jayander\Dropbox\Million\stop
+del C:\Users\Jayander\git\MillionMaker\MillionMaker\stop
+
+java com.million.BaseAlertManager -inputFile=C:/Users/Jayander/Dropbox/Million/Winners/OTA-WATCHLIST-INTRADAY.csv -repeatRuns=1 -stopAfterHours=23 -runEveryXMinutes=1 -alertRange=1
+
+java com.million.BaseAlertManager -inputFile=C:/Users/Jayander/Dropbox/Million/Winners/OTA-WATCHLIST-WIT.csv -repeatRuns=1 -stopAfterHours=23 -runEveryXMinutes=1 -alertRange=1
+
+java com.million.BaseAlertManager -inputFile=C:/Users/Jayander/Dropbox/Million/Winners/OTA-WATCHLIST.csv -repeatRuns=1 -stopAfterHours=23 -runEveryXMinutes=1 -alertRange=2
